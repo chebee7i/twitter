b89f2ce4ba39cfaf3b7e9d36a8ed43e80ecb34ca
chebee7i <chebee7i@gmail.com>
2014-09-16 19:09:00 -0500

    Add example of getting counts per state.

--- scripts/geo.py
+++ scripts/geo.py
@@ -12,13 +12,13 @@ db.hashtagTweets.find({coordinates: {$geoWithin : { $geometry : {type:
 
 
 """
+from __future__ import print_function
 
 import twitterproj
+import fiona
 
-if __name__ == '__main__':
-    tweets, db, conn = twitterproj.connect()
-
-    coords = [
+def seattle_counts(collection):
+    seattle = [
         [
             [-122.4596959,47.4810022],
             [-122.4596959,47.7341388],
@@ -27,4 +27,25 @@ if __name__ == '__main__':
             [-122.4596959,47.4810022]
         ]
     ]
-    counts, skipped = twitterproj.polygon_hashtags(db.hashtagTweets, coords)
+    counts, skipped = twitterproj.hashtag_counts_in(collection, seattle)
+    return counts, skipped
+
+def hashtag_counts_by_state(collection, shpfile):
+    hci = twitterproj.hashtag_counts_in
+
+    with fiona.open(shpfile, 'r') as f:
+        out = {}
+        for feature in f:
+            name = feature['properties']['NAME']
+            print(name)
+            geometry = feature['geometry']
+            counts, skipped = hci(collection, geometry)
+            out[name] = counts
+    return out
+
+
+if __name__ == '__main__':
+    db = twitterproj.connect()
+    #counts, skipped = seattle_counts(db.hashtagTweets)
+    counts = hashtag_counts_by_state(db.hashtagTweets,
+                                     '../tiger/tl_2014_us_state.shp')
